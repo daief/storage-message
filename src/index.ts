4385bcf7da2312d1e92ddac29f4af48f024cbf86
daief <1437931235@qq.com>
2018-09-21 16:26:09 +0800

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,58 @@
 // ------ declare
-const CURRENT_STORAGE = 'currentStorage'
+const CURRENT_STORAGE = 'currentStorage';
 
 // storage event 在当前页面不触发，重写 window.localStorage.setItem
 function overrideSetItem() {
-  const originalSetItem = window.localStorage.setItem
+  const originalSetItem = window.localStorage.setItem;
   window.localStorage.setItem = function(key, value) {
-    originalSetItem.apply(this, [key, value])
+    originalSetItem.apply(this, [key, value]);
 
-    const oldValue = window.localStorage.getItem(key)
+    const oldValue = window.localStorage.getItem(key);
     if (oldValue === `${value}`) {
-      return
+      return;
     }
 
     const setItemEvent = new StorageEvent(CURRENT_STORAGE, {
       key,
       oldValue,
-      newValue: value
-    })
+      newValue: value,
+    });
 
-    window.dispatchEvent(setItemEvent)
-  }
+    window.dispatchEvent(setItemEvent);
+  };
 }
 
-type IListener = (e: StorageEvent) => void
+type IListener = (e: StorageEvent) => void;
 interface IEvent {
-  key: string
-  listener: IListener
+  key: string;
+  listener: IListener;
 }
 
 class StorageMessage {
-  events: (IEvent| null)[] = []
+  events: Array<IEvent | null> = [];
 
   addEventListener(key: string, listener: IListener) {
-    this.events.push({key, listener})
+    this.events.push({ key, listener });
   }
 
   removeEventListener(key: string, listener: IListener) {
     for (let index = 0; index < this.events.length; index++) {
-      const element = this.events[index]
+      const element = this.events[index];
       if (element && element.key === key && element.listener === listener) {
-        this.events[index] = null
+        this.events[index] = null;
       }
     }
 
-    this.events = this.events.filter(e => e)
+    this.events = this.events.filter(e => e);
   }
 
   _notice(e: StorageEvent) {
-    const {key} = e
+    const { key } = e;
     this.events.forEach(item => {
       if (item && typeof item.listener === 'function' && key === item.key) {
-        item.listener(e)
+        item.listener(e);
       }
-    })
+    });
   }
 
   trigger(key: string, value: any) {
@@ -63,30 +63,24 @@ class StorageMessage {
         data: value,
         timestap: Date.now(),
       })
-    )
+    );
   }
 
   storageChange(e: StorageEvent) {
-    this._notice(e)
+    this._notice(e);
   }
 }
 
-
 // ------ excute
 
-overrideSetItem()
-const storageMessage = new StorageMessage()
+overrideSetItem();
+const storageMessage = new StorageMessage();
 const storageChange = (e: StorageEvent) => {
-  storageMessage._notice(e)
-}
+  storageMessage._notice(e);
+};
 
-window.addEventListener(CURRENT_STORAGE, storageChange)
-window.addEventListener('storage', storageChange)
+window.addEventListener(CURRENT_STORAGE, storageChange);
+window.addEventListener('storage', storageChange);
 
 // ------ export
-export {
-  IListener,
-  IEvent,
-  StorageMessage,
-  storageMessage
-}
+export { IListener, IEvent, StorageMessage, storageMessage };
