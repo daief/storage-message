a998ba06778ae80292f7a42dc2bf2b4afc4382f0
daief <1437931235@qq.com>
2018-09-21 15:52:58 +0800

    add code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// ------ declare
+const CURRENT_STORAGE = 'currentStorage'
+
 // storage event 在当前页面不触发，重写 window.localStorage.setItem
 function overrideSetItem() {
   const originalSetItem = window.localStorage.setItem
@@ -9,7 +12,7 @@ function overrideSetItem() {
       return
     }
 
-    const setItemEvent = new StorageEvent('currentStorage', {
+    const setItemEvent = new StorageEvent(CURRENT_STORAGE, {
       key,
       oldValue,
       newValue: value
@@ -17,4 +20,73 @@ function overrideSetItem() {
 
     window.dispatchEvent(setItemEvent)
   }
+}
+
+type IListener = (e: StorageEvent) => void
+interface IEvent {
+  key: string
+  listener: IListener
+}
+
+class StorageMessage {
+  events: (IEvent| null)[] = []
+
+  addEventListener(key: string, listener: IListener) {
+    this.events.push({key, listener})
+  }
+
+  removeEventListener(key: string, listener: IListener) {
+    for (let index = 0; index < this.events.length; index++) {
+      const element = this.events[index]
+      if (element && element.key === key && element.listener === listener) {
+        this.events[index] = null
+      }
+    }
+
+    this.events = this.events.filter(e => e)
+  }
+
+  _notice(e: StorageEvent) {
+    const {key} = e
+    this.events.forEach(item => {
+      if (item && typeof item.listener === 'function' && key === item.key) {
+        item.listener(e)
+      }
+    })
+  }
+
+  trigger(key: string, value: any) {
+    // auto add timestap
+    window.localStorage.setItem(
+      key,
+      JSON.stringify({
+        data: value,
+        timestap: Date.now(),
+      })
+    )
+  }
+
+  storageChange(e: StorageEvent) {
+    this._notice(e)
+  }
+}
+
+
+// ------ excute
+
+overrideSetItem()
+const storageMessage = new StorageMessage()
+const storageChange = (e: StorageEvent) => {
+  storageMessage._notice(e)
+}
+
+window.addEventListener(CURRENT_STORAGE, storageChange)
+window.addEventListener('storage', storageChange)
+
+// ------ export
+export {
+  IListener,
+  IEvent,
+  StorageMessage,
+  storageMessage
 }
