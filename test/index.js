aa3a4228b6aacc60bcbd4d7b0bacc495f5612315
daief <1437931235@qq.com>
2018-09-21 18:51:23 +0800

    add karma

---
+++ test/index.js
@@ -0,0 +1,44 @@
+// const {storageMessage} = require('../lib/index')
+// const chai = require('chai')
+// const {expect} = chai
+describe('#storage-message', () => {
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
+
+  it('test localStorage', (done) => {
+    console.log(123);
+    window.localStorage.setItem('test', 'test');
+    expect(window.localStorage.getItem('test')).to.equal('test');
+    done();
+  });
+
+  it('test', (done) => {
+    expect(1).to.equal(1);
+    done();
+  });
+
+  // it('test localStorage storage', (done) => {
+  //   window.localStorage.setItem('test', 'test')
+  //   window.addEventListener('storage', (e) => {
+  //     console.log(e)
+  //     expect(e).not.to.be.null
+  //     done()
+  //   })
+  // })
+
+  // it('addEventListener: watch1, send msg: data1', done => {
+  //   storageMessage.addEventListener('watch1', (e) => {
+  //     const {key, oldValue, newValue} = e
+  //     const obj = JSON.parse(newValue)
+  //     expect(key).to.equal('watch1')
+  //     expect(oldValue).to.be.null
+  //     expect(obj.data).to.equal('data1')
+  //     expect(obj.timestap).to.be.a('number')
+  //     done()
+  //   })
+
+  //   expect(storageMessage.events.length).to.equal(1)
+  //   storageMessage.trigger('watch1', 'data1')
+  // })
+});
