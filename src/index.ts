f40a72b5dd7ff9e08ff79b5bcf989678b5c69744
daief <1437931235@qq.com>
2018-09-21 14:26:23 +0800

    add overrideSetItem

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-export const lib = 'storage-message'
+// storage event 在当前页面不触发，重写 window.localStorage.setItem
+function overrideSetItem() {
+  const originalSetItem = window.localStorage.setItem
+  window.localStorage.setItem = function(key, value) {
+    originalSetItem.apply(this, [key, value])
+
+    const oldValue = window.localStorage.getItem(key)
+    if (oldValue === `${value}`) {
+      return
+    }
+
+    const setItemEvent = new StorageEvent('currentStorage', {
+      key,
+      oldValue,
+      newValue: value
+    })
+
+    window.dispatchEvent(setItemEvent)
+  }
+}
